9708428d04b0a0939e220dd9d22bc3b22785a742
Freod <radek.paradecki@gmail.com>
2022-04-15 17:48:10 +0200

    added database connection, ddl, docker-compose

---
+++ src/main/java/com/flowersAndGifts/database/DatabaseConnection.java
@@ -0,0 +1,46 @@
+package com.flowersAndGifts.database;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.*;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static final DatabaseConnection instance = new DatabaseConnection();
+
+    public static DatabaseConnection getInstance() {
+        return instance;
+    }
+
+    public Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
+
+    private DatabaseConnection() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+                try (Statement statement = conn.createStatement()) {
+                    statement.execute(getSql("database/init-ddl.sql"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getSql(final String resourceName) {
+        return new BufferedReader(
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                DatabaseConnection.class.getClassLoader().getResourceAsStream(resourceName))))
+                .lines()
+                .collect(Collectors.joining("\n"));
+    }
+}
