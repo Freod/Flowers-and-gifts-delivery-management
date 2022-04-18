05593c29e76d2a5fa2eed9ebf9084ae35e6e0493
Freod <radek.paradecki@gmail.com>
2022-04-18 19:58:43 +0200

    AbstractDao refactor

--- src/main/java/com/flowersAndGifts/dao/impl/AbstractDao.java
+++ src/main/java/com/flowersAndGifts/dao/impl/AbstractDao.java
@@ -1,6 +1,7 @@
 package com.flowersAndGifts.dao.impl;
 
 import com.flowersAndGifts.database.DatabaseConnection;
+import com.flowersAndGifts.exception.DaoException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,15 +10,9 @@ import java.util.List;
 
 public abstract class AbstractDao {
     private final DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-    public Connection connection;
 
-    public AbstractDao() {
-        //TODO:change it
-        try {
-            this.connection = databaseConnection.createConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    protected Connection getConnection() throws SQLException {
+        return databaseConnection.createConnection();
     }
 
     protected PreparedStatement getPreparedStatement(final Connection connection, final String query, final List<Object> parameters) throws SQLException {
@@ -28,24 +23,20 @@ public abstract class AbstractDao {
 
     protected void setPreparedStatementParameters(final PreparedStatement preparedStatement, final List<Object> parameters) throws SQLException {
         for (int i = 1; i <= parameters.size(); i++) {
-            setPreparedStatementParameter(preparedStatement, i, parameters.get(i-1));
+            setPreparedStatementParameter(preparedStatement, i, parameters.get(i - 1));
         }
     }
 
     protected void setPreparedStatementParameter(PreparedStatement preparedStatement, int index, Object parameter) throws SQLException {
-        if(Long.class == parameter.getClass()){
+        if (Long.class == parameter.getClass()) {
             preparedStatement.setLong(index, (Long) parameter);
-        }
-        else if(Integer.class == parameter.getClass()){
+        } else if (Integer.class == parameter.getClass()) {
             preparedStatement.setInt(index, (Integer) parameter);
-        }
-        else if(String.class == parameter.getClass()){
+        } else if (String.class == parameter.getClass()) {
             preparedStatement.setString(index, (String) parameter);
-        }
-        else if(Boolean.class == parameter.getClass()){
+        } else if (Boolean.class == parameter.getClass()) {
             preparedStatement.setBoolean(index, (Boolean) parameter);
-        }
-        else if(Double.class == parameter.getClass()){
+        } else if (Double.class == parameter.getClass()) {
             preparedStatement.setDouble(index, (Double) parameter);
         }
     }
